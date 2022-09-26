5e1bc84bf1364a1ef1a7c6ed67f67efea1b7118c
olamiye <captainjoe550@gmail.com>
2022-09-26 19:34:01 +0100

    Description of some indexjs files

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const labels = document.querySelectorAll('.form-control label')
 
 labels.forEach( label =>{
-    label.innerHTML = label.innerText
-        .split('')
-        .map((letter, idx) => `<span>${letter}</span>` )
-        .join('')
+    label.innerHTML = label.innerText //This how the original array of Email looks like ['EMAIL']
+        .split('')//This get the innerText of the label and turns the string to an array ['E', 'M', 'A' 'I', 'L']
+        .map((letter, idx) => `<span>${letter}</span>` )// This maps over the array and creates a new version of the array, 
+        //but with spans attached to each of those letter in the array  
+        //['<span>E<span>', '<span>M</span>', '<span>A</span>' '<span>I'</span>, '<span>L</span>']
+        .join('') //This basically joins all the span items in the array together
 })
